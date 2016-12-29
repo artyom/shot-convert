a5ad2e88fdf9d02ef2fd093f37c04605ceb58d0c
Artyom Pervukhin <artyom@evasive.ru>
2016-12-29 17:58:21 +0300

    Initial commit

---
+++ shot-convert.go
@@ -0,0 +1,108 @@
+// Command shot-convert watches specified directory for new png screenshots and
+// convert them to jpeg images
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"image"
+	"image/jpeg"
+	_ "image/png"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"strings"
+	"time"
+
+	"github.com/artyom/autoflags"
+	"github.com/fsnotify/fsnotify"
+)
+
+func main() {
+	args := struct {
+		Dir    string `flag:"dir,directory to watch"`
+		Prefix string `flag:"prefix,file prefix"`
+	}{Dir: "$HOME/Downloads", Prefix: "Screen Shot"}
+	autoflags.Define(&args)
+	flag.Parse()
+	if err := do(os.ExpandEnv(args.Dir), args.Prefix); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}
+
+func do(dir, prefix string) error {
+	w, err := fsnotify.NewWatcher()
+	if err != nil {
+		return err
+	}
+	// defer w.Close() // https://github.com/fsnotify/fsnotify/issues/187
+	if err := w.Add(dir); err != nil {
+		return err
+	}
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	nameCh := make(chan string, 10)
+	errCh := make(chan error, 1)
+	go func() {
+		for {
+			select {
+			case name := <-nameCh:
+				time.Sleep(500 * time.Millisecond)
+				if err := convert(name); err != nil {
+					errCh <- err
+				}
+			case <-ctx.Done():
+				return
+			}
+		}
+	}()
+	for {
+		select {
+		case err := <-errCh:
+			return err
+		case evt := <-w.Events:
+			if evt.Op&fsnotify.Create != fsnotify.Create {
+				continue
+			}
+			if b := filepath.Base(evt.Name); strings.HasPrefix(b, prefix) &&
+				strings.HasSuffix(b, suffix) {
+				nameCh <- evt.Name
+			}
+		case err := <-w.Errors:
+			return err
+		}
+	}
+}
+
+func convert(name string) error {
+	jpegName := strings.TrimSuffix(name, suffix) + ".jpg"
+	if _, err := os.Stat(jpegName); err == nil {
+		return nil
+	}
+	f, err := os.Open(name)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+	img, _, err := image.Decode(f)
+	if err != nil {
+		return err
+	}
+	tf, err := ioutil.TempFile(filepath.Dir(name), ".shot-convert-")
+	if err != nil {
+		return err
+	}
+	defer tf.Close()
+	defer os.Remove(tf.Name())
+	if err := jpeg.Encode(tf, img, &jpeg.Options{Quality: 90}); err != nil {
+		return err
+	}
+	if err := tf.Close(); err != nil {
+		return err
+	}
+	return os.Rename(tf.Name(), jpegName)
+}
+
+const suffix = ".png"
